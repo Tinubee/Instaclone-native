d411d30f4cec91a99ff98dee9b8b38595af775db
KIMHYOUNGMIN <rlagud2005@naver.com>
2022-05-18 00:57:18 +0900

    First Commit

---
+++ screens/CreateAccount.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+export default function CreateAccount() {
+  return <Container></Container>;
+}
